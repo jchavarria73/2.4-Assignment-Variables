533994aa10d9c57ae68499aaef4fb1cf91f397c6
jchavarria73 <jchavarria73@student.alamo.edu>
2023-09-06 15:24:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+var x = "Global"; // Global Variable
+console.log(x);
+
+{
+    let y = "Block"; // Block Variable
+    console.log(y)
+}
+
 var life = 100; // This is my life bar NUMBER
 
 life = life - 10;
 
 //90
 
-life = life / 10;
+life = life - 10;
 
-var name = "Dev Ed"; // This is the name displayed in the game STRING
+console.log(life);
 
-var checkout = false; // BOOLEAN
+var name = "Jose"; // This is the name displayed in the game String Variable
+console.log(name);
 
-var box = null;
-
-//OBJECT SYNBOL
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car);
 
 console.log(box);
