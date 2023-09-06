7d1670d4102c8a20dba19a1fb85673efe398a632
jchavarria73 <jchavarria73@student.alamo.edu>
2023-09-06 14:16:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var life = 100; // This is my life bar NUMBER
 
+life = life - 10;
+
+//90
+
+life = life / 10;
+
 var name = "Dev Ed"; // This is the name displayed in the game STRING
 
-console.log(life);
+var checkout = false; // BOOLEAN
+
+var box = null;
+
+//OBJECT SYNBOL
+
+console.log(box);
